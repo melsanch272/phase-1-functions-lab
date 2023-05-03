9a4a60980b554bc50d94fa81dea9c00a73f2fdde
Melissa Sanchez <melissasanchez064@gmail.com>
2023-05-03 15:11:12 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    const headquaters = 42
+    return Math.abs(someValue - headquaters)
+}
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue)*264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (destination > start) {
+        return ((destination - start)*264);
+    } 
+    else
+        return ((start - destination)*264);
+    
+}
+
+function calculatesFarePrice(start, destination) {
+    const travel = distanceTravelledInFeet(start, destination);
+    if ( travel< 400) {
+        return 0
+    }
+    else if (travel> 400 && travel <= 2000) {
+        return (travel-400)*.02
+    }
+    else if (travel > 2000 && travel <= 2500) {
+        return 25
+    }
+    else if (travel > 2500) {
+        return 'cannot travel that far'
+    }
+
+}
